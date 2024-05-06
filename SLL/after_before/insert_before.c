351eb12965af8947186084205e21a1f0b3a3ec5d
pavithracr1111 <pavithrar.19.beee@acharya.ac.in>
2024-05-07 00:06:47 +0530

    addede comments

--- SLL/after_before/insert_before.c
+++ SLL/after_before/insert_before.c
@@ -2,37 +2,37 @@
 
 int sl_insert_before(Slist **head, data_t g_data, data_t ndata) // function to insert before a given node
 {
-    if(head == NULL)
+    if (head == NULL) // if list is empty
     {
         return LIST_EMPTY;
     }
     else
     {
-       Slist *temp = *head;
-       Slist *prev = NULL;
-       
-       while(temp != NULL)
-       {
-        if(temp->data != g_data)
+        Slist *temp = *head; // create a local reference pointer
+        Slist *prev = NULL;  // create a another local pointer to store previous address
+
+        while (temp != NULL)
         {
-            prev=temp;
-            temp=temp->link;
-        }
-        else
-        {
-            Slist *new=malloc(sizeof(Slist));
-            if(new==NULL)
+            if (temp->data != g_data) // checking the given data is present in the node or not
             {
-                return FAILURE;
+                prev = temp;       // store temp address before traversing in prev pointer
+                temp = temp->link; // if the given data not present in the list, traverse the temp
             }
 
-            new->data=ndata;
-            new->link=prev->link;
-            prev->link=new;
-            return SUCCESS;
+            else
+            {
+                Slist *new = malloc(sizeof(Slist)); // allocate memory for new node
+                if (new == NULL)                    // do error check for memory allocated or not for new node
+                {
+                    return FAILURE;
+                }
+
+                new->data = ndata;      // update new data with the given data to add
+                new->link = prev->link; // establish the link b/w new node and previous node(update new link with the prev link to insert before)
+                prev->link = new;       // establish the link b/w prev and new (update prev link with the new add)
+                return SUCCESS;
+            }
         }
-       }
-return DATA_NOT_FOUND;
-    
+        return DATA_NOT_FOUND; // if the given data not present in the list, return failure mess
     }
 }
