afe2469586df253b5cbea5a894eaa8bdfa84fabd
pavithracr1111 <pavithrar.19.beee@acharya.ac.in>
2024-05-20 23:11:03 +0530

    did delete element in dll

--- DLL/after_before/delete_element.c
+++ DLL/after_before/delete_element.c
@@ -2,6 +2,30 @@
 
 int dl_delete_element(Dlist **head, Dlist **tail, int data)
 {	
-   
-
+    Dlist *temp = *head;
+    if(*head == NULL)
+    {
+        return LIST_EMPTY;
+    }
+    if(*head == *tail)
+    {
+        *head = *tail = NULL;
+        free(temp);
+        return SUCCESS;
+    }
+    else
+    {
+        while(temp != NULL)
+        {
+            if(temp -> data == data)
+            {
+                temp -> prev -> next = temp -> next;
+                temp -> next = temp -> prev;
+                free(temp);
+            }
+            temp = temp -> next;
+        }
+        return SUCCESS;
+    }
+    return DATA_NOT_FOUND;
 }
